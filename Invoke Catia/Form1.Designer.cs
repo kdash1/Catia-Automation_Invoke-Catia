c0763d800b5bb0ebf3cd43e35f3adb5c8ef2553b
kdash1 <kalidashp2@gmail.com>
2024-03-23 23:06:05 +0530

    Add project files.

---
+++ Invoke Catia/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Invoke_Catia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInvoke = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInvoke
+            // 
+            this.btnInvoke.Location = new System.Drawing.Point(48, 43);
+            this.btnInvoke.Name = "btnInvoke";
+            this.btnInvoke.Size = new System.Drawing.Size(191, 41);
+            this.btnInvoke.TabIndex = 0;
+            this.btnInvoke.Text = "Invoke Catia";
+            this.btnInvoke.UseVisualStyleBackColor = true;
+            this.btnInvoke.Click += new System.EventHandler(this.btnInvoke_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(48, 103);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(191, 41);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add New Part";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 183);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnInvoke);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInvoke;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Invoke Catia/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using INFITF;
+using PARTITF;
+using MECMOD;
+
+
+namespace Invoke_Catia
+{
+    public partial class Form1 : Form
+    {
+        INFITF.Application catiaApp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInvoke_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                catiaApp = (INFITF.Application)Marshal.GetActiveObject("CATIA.Application");
+            }
+            catch
+            {
+                Type catiaType = Type.GetTypeFromProgID("CATIA.Application");
+                catiaApp = (INFITF.Application)Activator.CreateInstance(catiaType);
+            }
+            catiaApp.Visible = true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            INFITF.Document catiaDoc;
+            if (catiaApp != null)
+            {
+                catiaDoc = catiaApp.Documents.Add("Part");
+                MessageBox.Show("New Part Document added", "Catia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+        }
+    }
+}
